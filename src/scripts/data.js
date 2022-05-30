e6e5f48f0ffbcaef6eae9772286fccf3110f2618
Michael Chen <michael.chen.1120@gmail.com>
2022-05-29 21:19:24 -0400

    Add race class add placeholder cache function for api calls

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,1 +1,22 @@
 //get data for: race results, laps, pitstops, drivers
+
+const cacheTime = 10000;
+const cache = {};
+let cacheTimer = 0;
+
+const getCacheTimer = time => {
+  const currentTime = new Date().getTime();
+  if (cacheTimer < currentTime + time) {
+    cacheTimer = currentTime + time;
+  }
+  return cacheTimer;
+}
+
+const fetchWithCache = async () => {
+  const currentTime = new Date().getTime();
+  if (!cache() || cache[].cacheTimer < currentTime) {
+    cache[] = await fetchRaceDate();
+    cache[].cacheTimer = getCacheTimer(time);
+  }
+  return cache[];
+}
---
+++ src/scripts/race.js
@@ -0,0 +1,9 @@
+//Add race class
+class Race {
+  constructor(raceResults, laps, pitstops, drivers) {
+    this.raceResults = raceResults;
+    this.laps = laps;
+    this.pitstops = pitstops;
+    this.drivers = drivers;
+  }
+}
