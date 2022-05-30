afe697a792a6c8446c9d16c43f1529ca42536715
Michael Chen <michael.chen.1120@gmail.com>
2022-05-30 12:15:38 -0400

    Adding function to iterate though urls for api calls

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,22 +1,43 @@
 //get data for: race results, laps, pitstops, drivers
 
-const cacheTime = 10000;
-const cache = {};
-let cacheTimer = 0;
+// const cacheTime = 1000000;
+// const cache = {};
+// let cacheTimer = 0;
 
-const getCacheTimer = time => {
-  const currentTime = new Date().getTime();
-  if (cacheTimer < currentTime + time) {
-    cacheTimer = currentTime + time;
-  }
-  return cacheTimer;
+// const getCacheTimer = time => {
+//   const currentTime = new Date().getTime();
+//   if (cacheTimer < currentTime + time) {
+//     cacheTimer = currentTime + time;
+//   }
+//   return cacheTimer;
+// }
+
+// const fetchWithCache = async (time) => {
+//   const currentTime = new Date().getTime();
+//   if (!cache() || cache[].cacheTimer < currentTime) {
+//     cache[] = await fetchRaceDate();
+//     cache[].cacheTimer = getCacheTimer(time);
+//   }
+//   return cache[];
+// }
+
+// const raceResults = fetch('https://ergast.com/api/f1/current/results.json?limit=30&offset=120')
+//   .then(result => result.json())
+//   .then(data => console.log(data))
+
+//fetch race results for season
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
 }
 
-const fetchWithCache = async () => {
-  const currentTime = new Date().getTime();
-  if (!cache() || cache[].cacheTimer < currentTime) {
-    cache[] = await fetchRaceDate();
-    cache[].cacheTimer = getCacheTimer(time);
+// const raceResultsURL = 'https://ergast.com/api/f1/current/results.json?limit=30&offset=120';
+
+const perRaceData = (apiParam) => {
+  let i = 1;
+  while (i < 8) {
+    let url = `https://ergast.com/api/f1/current/${i}/${apiParam}`;
+
   }
-  return cache[];
 }
+
