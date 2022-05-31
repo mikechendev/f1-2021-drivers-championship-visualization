786d08c5c10fde8bc82cbe3f264f96b413c35e16
Michael Chen <michael.chen.1120@gmail.com>
2022-05-31 02:52:16 -0400

    Add basic chartjs boilerplate

---
+++ src/scripts/chart.js
@@ -0,0 +1,13 @@
+import * as d3 from "d3";
+
+export function chart() {
+  const chartSettings = {
+    width: 600,
+    height: 400,
+    padding: 40,
+    titlePadding: 5,
+    columnPadding: 0.4,
+    ticksInXAxis: 5,
+    duration: 3500
+  }
+}
