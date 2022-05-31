f416ec8ed171588bcbcfa80868ee3435f3ceb632
Michael Chen <michael.chen.1120@gmail.com>
2022-05-31 18:40:17 -0400

    add driver photos to assets folder

--- src/scripts/chart.js
+++ src/scripts/chart.js
@@ -12,29 +12,7 @@ let config = {
 };
 
 const raceData = {
-  labels: [
-    'Leclerc',
-    'Verstappen',
-    'Sainz',
-    'Hamilton',
-    'Magnussen',
-    'Perez',
-    'Russell',
-    'Gasly',
-    'Alonso',
-    'Ocon',
-    'Albon',
-    'Tsuonoda',
-    'Schumacher',
-    'Bottas',
-    'Hulkenberg',
-    'Norris',
-    'Stroll',
-    'Latifi',
-    'Zhou',
-    'Ricciardo',
-    'Vettel',
-  ],
+  labels: [],
   datasets: [
     {
       label: 'Points',
@@ -47,14 +25,6 @@ const raceData = {
         'rgba(75, 192, 192, 0.2)',
         'rgb(30,91,198)',
         'rgb(108,211,191)',
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)',
       ],
       borderWidth: 1,
     },
