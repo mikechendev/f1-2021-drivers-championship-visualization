3fcc011e7e30d7ce56057f7268a5c8db8966ee56
Michael Chen <michael.chen.1120@gmail.com>
2022-09-01 19:30:52 -0400

    Add race class

--- src/scripts/race.js
+++ src/scripts/race.js
@@ -1,6 +1,10 @@
 export default class Race {
-  constructor(name) {
-    this.name = name;
-    this.results = []
+  constructor(result) {
+    this.round = result.round;
+    this.raceName = result.raceName;
+    this.circuit = result.Circuit.circuitName;
+    this.circuitUrl = result.Circuit.url;
+    this.country = result.Circuit.country;
+    this.date = result.date;
   }
 }
