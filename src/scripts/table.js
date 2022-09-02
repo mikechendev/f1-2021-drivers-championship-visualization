24ab1d40e7ca34c9eb9b30688a54b862f6f53ea0
Michael Chen <michael.chen.1120@gmail.com>
2022-09-02 19:36:38 -0400

    Format and style table header

--- src/scripts/table.js
+++ src/scripts/table.js
@@ -10,20 +10,37 @@ export const createTable = (value) => {
   const races = createRaces();
   const race = races.find((race) => value == race.round);
 
+  let tableContainer = document.createElement('div');
   let titleDiv = document.createElement('div');
   let dateDiv = document.createElement('div');
   let locationDiv = document.createElement('div');
+  let circuitLink = document.createElement('a');
+  let circuitDiv = document.createElement('div');
+
   let table = document.createElement('table');
   let th = document.createElement('th');
   let td = document.createElement('td');
 
+  tableContainer.classList.add('table-container');
+
   titleDiv.classList.add('title-div');
   titleDiv.innerText = race.raceName;
+  tableContainer.append(titleDiv);
+
+  circuitDiv.classList.add('circuit-div');
+  circuitDiv.innerText = race.circuit;
+  circuitLink.href = race.circuitUrl;
+  circuitLink.append(circuitDiv);
+  tableContainer.append(circuitLink);
+
   dateDiv.classList.add('info-div');
   dateDiv.innerText = race.date;
+  tableContainer.append(dateDiv);
+
   locationDiv.classList.add('location-div');
   locationDiv.innerText = `${race.locality}, ${race.country}`;
+  tableContainer.append(locationDiv);
 
   clearDiv();
-  chartContainer.append(titleDiv, dateDiv, locationDiv);
+  chartContainer.append(tableContainer);
 };
