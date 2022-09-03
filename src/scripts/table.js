7561091d97ee35a73cc4c9f7f64804a2551808a5
Michael Chen <michael.chen.1120@gmail.com>
2022-09-03 04:38:29 -0400

    Finish table and basic styling

--- src/scripts/table.js
+++ src/scripts/table.js
@@ -45,11 +45,33 @@ export const createTable = (value) => {
   // loop for table data
 
   for (let i = 0; i < race.results.length; i++) {
-    tbody.append(document.createElement('tr'));
-  }
+    let newTr = document.createElement('tr');
+    let pos = document.createElement('td');
+    pos.innerText = race.results[i].position;
+    let grid = document.createElement('td');
+    grid.innerText = race.results[i].grid;
+    let num = document.createElement('td');
+    num.innerText = race.results[i].number;
+    let driver = document.createElement('td');
+    let driverUrl = document.createElement('a');
+    driverUrl.href = race.results[i].Driver.url;
+    driverUrl.innerText = `${race.results[i].Driver.givenName} ${race.results[i].Driver.familyName}`;
+    driver.append(driverUrl);
+    let constructor = document.createElement('td');
+    let constructorUrl = document.createElement('a');
+    constructorUrl.href = race.results[i].Constructor.url;
+    constructorUrl.innerText = race.results[i].Constructor.name;
+    constructor.append(constructorUrl);
+    let laps = document.createElement('td');
+    laps.innerText = race.results[i].laps;
+    let time = document.createElement('td');
+    time.innerText = race.results[i].Time ? race.results[i].Time.time : '-';
+    let points = document.createElement('td');
+    points.innerText = race.results[i].points;
 
-  let trs = tbody.querySelectorAll('tr');
-  console.log(trs);
+    newTr.append(pos, grid, num, driver, constructor, laps, time, points);
+    tbody.append(newTr);
+  }
 
   tableContainer.classList.add('table-container');
 
@@ -82,7 +104,7 @@ export const createTable = (value) => {
   table.append(tbody);
 
   clearDiv();
-  tableContainer.append(titleDiv, circuitLink, dateDiv, locationDiv, table);
+  tableContainer.append(circuitLink, dateDiv, locationDiv, table);
   chartContainer.append(tableContainer);
 
   console.log(race);
